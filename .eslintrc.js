dc10024e55a70575b2880267dccdab347a08640d
matt-hu <matt.hu1@outlook.com>
2022-10-30 20:26:55 +0000

    Update workflows

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
@@ -10,5 +10,8 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    semi: ["warn", "always"]
   }
-}
+};

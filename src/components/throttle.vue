b518c95b131f54f32c7b67c2c5d55c4b794389a8
dexslab <dex35803@gmail.com>
2023-01-08 22:18:17 -0500

    linting is complete

--- src/components/throttle.vue
+++ src/components/throttle.vue
@@ -1,17 +1,26 @@
 <template>
   <v-container fluid>
     <v-card>
-      <v-tabs v-model="tab" color="primary">
+      <v-tabs
+        v-model="tab"
+        color="primary"
+      >
         <v-tab value="throttle">
-          <v-icon start> mdi:train-variant </v-icon>
+          <v-icon start>
+            mdi:train-variant
+          </v-icon>
           Throttle
         </v-tab>
         <v-tab value="cv">
-          <v-icon start> mdi:memory </v-icon>
+          <v-icon start>
+            mdi:memory
+          </v-icon>
           CVs
         </v-tab>
         <v-tab value="settings">
-          <v-icon start> mdi:cog-box </v-icon>
+          <v-icon start>
+            mdi:cog-box
+          </v-icon>
           Settings
         </v-tab>
       </v-tabs>
@@ -23,29 +32,31 @@
                 <v-col cols="3">
                   <control-knob
                     v-if="locomotive.throttleType == ThrottleType.ROTARY"
-                  ></control-knob>
+                  />
                   <v-slider
                     v-if="locomotive.throttleType == ThrottleType.VERTICAL"
                     direction="vertical"
-                  ></v-slider>
+                  />
                   <v-slider
                     v-if="locomotive.throttleType == ThrottleType.HORIZONTAL"
-                  ></v-slider>
+                  />
                 </v-col>
-                <v-col cols="6"> Function buttons </v-col>
+                <v-col cols="6">
+                  Function buttons
+                </v-col>
               </v-row>
             </v-col>
           </v-row>
         </v-window-item>
 
-        <v-window-item value="cv"> </v-window-item>
+        <v-window-item value="cv"/>
         <v-window-item value="settings">
           <v-combobox
             v-model="locomotive.throttleType"
             label="speedController"
             variant="solo"
-            @update:model-value="updateActiveThrottle(loco, locomotive)"
             :items="['Vertical', 'Knob', 'Circular', 'Horizontal']"
+            @update:model-value="updateActiveThrottle(loco, locomotive)"
           />
         </v-window-item>
       </v-window>
@@ -54,20 +65,20 @@
 </template>
 <script lang="ts">
 export default {
-  name: "Throttle",
+  name: 'Throttle',
 };
 </script>
 <script setup lang="ts">
-import ControlKnob from "@slipmatio/control-knob";
-import { storeToRefs } from "pinia";
-import { ref, computed, defineProps } from "vue";
-import { ThrottleType, savedLocosStore } from "../store/locos";
+import ControlKnob from '@slipmatio/control-knob';
+import {storeToRefs} from 'pinia';
+import {ref, computed, defineProps} from 'vue';
+import {ThrottleType, savedLocosStore} from '../store/locos';
 
 const locoStore = savedLocosStore();
-const props = defineProps({ loco: Number });
-const { activeThrottles } = storeToRefs(locoStore);
+const props = defineProps({loco: Number});
+const {activeThrottles} = storeToRefs(locoStore);
 const updateActiveThrottle = computed(() => locoStore.updateActiveThrottle);
 const locomotive = activeThrottles.value[props.loco ? props.loco : 0];
-const tab = ref("throttle");
+const tab = ref('throttle');
 </script>
 <style lang="scss" scoped></style>
--- src/views/Throttles.vue
+++ src/views/Throttles.vue
@@ -2,17 +2,29 @@
   <v-container fluid>
     <v-row>
       <v-col cols="2">
-        <v-tabs v-model="activeThrottle" direction="vertical" color="primary">
-          <v-tab v-for="(l, k) in activeThrottles" :value="k">
-            <v-icon start> mdi:train-variant </v-icon>
+        <v-tabs
+          v-model="activeThrottle"
+          direction="vertical"
+          color="primary"
+        >
+          <v-tab
+            v-for="(l, k) in activeThrottles"
+            :value="k"
+          >
+            <v-icon start>
+              mdi:train-variant
+            </v-icon>
             {{ l.displayName }}
           </v-tab>
         </v-tabs>
       </v-col>
       <v-col>
         <v-window v-model="activeThrottle">
-          <v-window-item v-for="(l, k) in activeThrottles" :value="k">
-            <throttle :loco="k"></throttle>
+          <v-window-item
+            v-for="(l, k) in activeThrottles"
+            :value="k"
+          >
+            <throttle :loco="k"/>
           </v-window-item>
         </v-window>
       </v-col>
@@ -21,16 +33,16 @@
 </template>
 <script lang="ts">
 export default {
-  name: "ThrottlePage",
+  name: 'ThrottlePage',
 };
 </script>
 <script setup lang="ts">
-import Throttle from "../components/throttle.vue";
-import { storeToRefs } from "pinia";
-import { ref, computed } from "vue";
-import { savedLocosStore } from "../store/locos";
+import Throttle from '../components/throttle.vue';
+import {storeToRefs} from 'pinia';
+import {ref, computed} from 'vue';
+import {savedLocosStore} from '../store/locos';
 const locoStore = savedLocosStore();
-const { activeThrottles } = storeToRefs(locoStore);
+const {activeThrottles} = storeToRefs(locoStore);
 
 const activeThrottle = ref(-1);
 </script>

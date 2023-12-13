298f2d8eaa55da4ecdaf000d67549b53165403aa
David Young <davie668@gmail.com>
2023-12-13 07:51:21 +0000

    Added functionality to Power toggle (#28)
    * Added serial communication
    
    Sent and Received messages are added to the Logs
    
    * Added ability to Toggle MAIN Power
    
    Includes sending a Power command

--- src/App.vue
+++ src/App.vue
@@ -13,21 +13,32 @@
       </v-app-bar-title>
 
       <template #append>
-        <v-switch
-          v-model="powerModel"
-          hide-details
-          true-value="on"
-          false-value="off"
-          class="p-switch"
-        >
-          <template #label>
-            <label>Power {{ powerModel }}</label>
-            <v-icon
-              icon="mdi:power"
-              :class="powerModel == 'on' ? 'p-on' : 'p-off'"
-            />
-          </template>
-        </v-switch>
+        <div>
+          <form @submit.prevent="serialConnectionRequest">
+            <span v-if="isConnected">Connected ✅</span>
+
+            <button v-else type="submit">
+              Connect
+            </button>
+          </form>
+
+          <v-switch
+            v-model="powerModel"
+            hide-details
+            true-value="1"
+            false-value="0"
+            class="p-switch"
+          >
+            <template #label>
+
+              <label>Power {{ powerModel.value == 1 ? 'On' : 'Off' }}</label>
+              <v-icon
+                icon="mdi:power"
+                :class="powerModel.value == 1 ? 'p-on' : 'p-off'"
+              />
+            </template>
+          </v-switch>
+        </div>
       </template>
     </v-app-bar>
     <v-navigation-drawer v-model="showNavBar">
@@ -38,14 +49,14 @@
           :to="item.to"
         >
           <template #prepend>
-            <v-icon :icon="item.icon" />
+            <v-icon :icon="item.icon"/>
           </template>
           <v-list-item-title>{{ item.title }}</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
     <v-main>
-      <router-view />
+      <router-view/>
     </v-main>
     <v-footer height="25">
       <!-- <v-row no-gutters>
@@ -73,6 +84,9 @@ import {computed, ref} from 'vue';
 import {useGlobalStore} from './store/global';
 import {useSettingsStore} from './store/settings';
 import {storeToRefs} from 'pinia';
+import {useCommunicationsStore} from '@/store/communications';
+import {useThrottlesStore} from '@/store/throttles';
+import {Active} from '@cloudthrottle/dcc-ex--commands';
 
 const theme = useTheme();
 const globalStore = useGlobalStore();
@@ -80,32 +94,48 @@ const settingsStore = useSettingsStore();
 const {getTitle, showNavBar, getNavList} = storeToRefs(globalStore);
 const {getTheme} = storeToRefs(settingsStore);
 const toggleNav = computed(() => globalStore.toggleNavBar);
-const powerModel = ref('off');
 theme.global.name.value = getTheme.value;
+
+const {serialConnectionRequest} = useCommunicationsStore();
+const { isConnected } = storeToRefs(useCommunicationsStore())
+
+const throttlesStore = useThrottlesStore();
+const {mainPower} = storeToRefs(throttlesStore)
+
+const powerModel = computed({
+  get: () => mainPower,
+  set: (active: unknown) => {
+    throttlesStore.setMainPower(active as Active)
+  }
+});
+
 </script>
 
 <style lang="scss">
-.p-switch.v-switch .v-selection-control__wrapper{
+.p-switch.v-switch .v-selection-control__wrapper {
   display: none !important;
   width: 0;
 }
+
 .p-switch .v-label label {
   font-size: 14px;
   font-variation-settings: "wght" 200;
   color: #ffffff;
   text-transform: capitalize;
 }
+
 .p-switch .p-on {
-    color: #ffffff;
-    box-shadow: 0px 0px 6px 0px #71ff71;
-    background: #00e700;
-    border-radius: 16px;
-    padding: 4px;
-    height: 32px;
-    width: 32px;
-    margin: 8px;
-    border: 1px solid #71ff71;
+  color: #ffffff;
+  box-shadow: 0px 0px 6px 0px #71ff71;
+  background: #00e700;
+  border-radius: 16px;
+  padding: 4px;
+  height: 32px;
+  width: 32px;
+  margin: 8px;
+  border: 1px solid #71ff71;
 }
+
 .p-switch .p-off {
   color: #ffffff;
   background: red;
--- src/store/communications.ts
+++ src/store/communications.ts
@@ -1,24 +1,30 @@
 import {defineStore} from 'pinia';
+import {
+  createSerialConnection,
+} from '@cloudthrottle/dcc-ex--serial-communicator';
 
-export type LogItemKind = 'sent' | 'received';
-export type LogItem = {
-    message: string
-    kind: LogItemKind
-    timestamps: {
-        createdAt: Date
-    }
+type LogItemKind = 'sent' | 'received';
+type LogItem = {
+  message: string
+  kind: LogItemKind
+  timestamps: {
+    createdAt: Date
+  }
 }
+type Logs = LogItem[]
 
-export type Logs = LogItem[]
+type Writer = WritableStreamDefaultWriter<string> | null
 
 type CommunicationsState = {
-    logs: Logs
+  logs: Logs,
+  writer: Writer
 }
 
 export const useCommunicationsStore = defineStore('communications', {
   state: (): CommunicationsState => {
     return {
       logs: [],
+      writer: null,
     };
   },
   getters: {
@@ -28,19 +34,58 @@ export const useCommunicationsStore = defineStore('communications', {
     receivedLogs: ({logs}): Logs => {
       return logs.filter((logItem) => logItem.kind === 'received');
     },
+    isConnected: ({writer}): boolean => {
+      return !!writer;
+    },
   },
   actions: {
     sendMessage(message: string): void {
-      const newLogItem: LogItem = {message, timestamps: {createdAt: new Date}, kind: 'sent'};
+      console.debug('sendMessage', message);
+      const newLogItem: LogItem = {
+        message,
+        timestamps: {createdAt: new Date},
+        kind: 'sent',
+      };
+      this.writeToSerial(message);
       this.addLogItem(newLogItem);
     },
     fakeReadMessage(message: string): void {
-      const newLogItem: LogItem = {message, timestamps: {createdAt: new Date}, kind: 'received'};
+      console.debug('fakeReadMessage', message);
+      const newLogItem: LogItem = {
+        message,
+        timestamps: {createdAt: new Date},
+        kind: 'received',
+      };
+      this.addLogItem(newLogItem);
+    },
+    readHandler(message: string): void {
+      console.debug('readHandler', message);
+      const newLogItem: LogItem = {
+        message,
+        timestamps: {createdAt: new Date},
+        kind: 'received',
+      };
       this.addLogItem(newLogItem);
     },
     addLogItem(logItem: LogItem): void {
       const newLogItem = {...logItem};
       this.logs = [newLogItem, ...this.logs];
+    },
+    writeToSerial(message: string): void {
+      console.debug('writeToSerial', message);
+      if (this.isConnected && this.writer) {
+        console.debug('writeToSerial', 'writer.write', message);
+        this.writer.write(message);
+      }
+    },
+    async serialConnectionRequest(): Promise<void> {
+      try {
+        const {writer} = await createSerialConnection({readHandler: this.readHandler});
+        this.writer = writer;
+      } catch (e) {
+        console.debug('serialConnectionRequest', e);
+        return;
+      }
     },
   },
 });
